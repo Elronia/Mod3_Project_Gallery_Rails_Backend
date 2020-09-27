88b286feaf6a29720ef88a8bced66b15c9824925
pierrewebdev <patrick.pierre000@gmail.com>
2020-09-27 18:12:49 -0400

    Adding data for paintings in database

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@ User.reset_pk_sequence
 Painting.reset_pk_sequence
 Favorite.reset_pk_sequence
 
-#shift off the first element to get rid of the column names
+################################################################
+#method to parse painter csv file
 def parse_painter_csv_file
     csv_data = CSV.read("db/artists_for_seed_data.csv")
     csv_data.shift
@@ -19,7 +20,6 @@ def parse_painter_csv_file
     painter_object_array = []
     csv_data.each do |painter_row_arr|
         painter_row_arr.shift
-        byebug
         painter_object = {
             :name => painter_row_arr[0],
             :years => painter_row_arr[1],
@@ -33,40 +33,50 @@ def parse_painter_csv_file
     painter_object_array.flatten
 end
 
-painters = parse_painter_csv_file
-# byebug
-painters.each do |painter_hash| 
-    Painter.create!(painter_hash)
-end 
-
+###############################################################
 # parsing paintings csv
 def parse_painting_csv_file
     csv_data = CSV.read("db/csv_paintings.csv")
+    csv_data.shift
     # iterate over each element and send back a hash 
     # need to shift again at the beginning to get rid of id on the row
-    painter_object_array = []
-    csv_data.each do |painter_row_arr|
-        byebug
-        painter_row_arr.shift
-        painter_object = {
-            :name => painter_row_arr[0],
-            :years => painter_row_arr[1],
-            :genre => painter_row_arr[2],
-            :nationality => painter_row_arr[3],
-            :bio => painter_row_arr[4],
-            :painting_num => painter_row_arr[6]
+    painting_object_array = []
+    csv_data.each do |painting_row_arr|
+        painting_row_arr.shift
+        painting_object = {
+            :name => painting_row_arr[0],
+            :image => painting_row_arr[1],
+            :year => painting_row_arr[2],
+            :painter_id => painting_row_arr[3]
         }
-            painter_object_array.push(painter_object)  
+        painting_object_array.push(painting_object)  
     end
-    painter_object_array.flatten
+    painting_object_array.flatten
+end
+
+
+
+
+
+
+#################################################################
+#creating database records
+
+#creates painter records in db
+painters = parse_painter_csv_file
+painters.each do |painter_hash| 
+    Painter.create!(painter_hash)
+end
+
+#create painting records in db
+paintings = parse_painting_csv_file
+paintings.each do |painting_hash|
+    Painting.create!(painting_hash)
 end
 
 katherine = User.create(username: "Elronia")
 patrick = User.create(username: "pierre2")
 
-#painting1 = Painting.create(name: "The Jewess", year: "1908", image: "db/paintings_image/Amedeo Modigliani_paintings", painter_id: 1)
-
-test_painting = Painting.create(name:"Westminister Bridge", year:"1871", image:"https://res.cloudinary.com/interactive-gallery/image/upload/v1601168557/paintings_image/Claude%20Monet_paintings/1871_Westminster_Bridge_aka_The_Thames_below_Westminster_monet_claude_2_z4ehjy.jpg",painter:Painter.first)
 
 favorite1 = Favorite.create(painting_id: 1, user_id: 2)
 puts "I have seeded the database"
