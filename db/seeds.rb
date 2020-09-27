7abf724ca0b17a672793952a830ff69faadee400
Elronia <zar.catherine@gmail.com>
2020-09-27 17:23:14 -0400

    fixing bug in csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Painting.reset_pk_sequence
 Favorite.reset_pk_sequence
 
 #shift off the first element to get rid of the column names
-def parse_csv_file
+def parse_painter_csv_file
     csv_data = CSV.read("db/artists_for_seed_data.csv")
     csv_data.shift
     # iterate over each element and send back a hash 
@@ -19,6 +19,7 @@ def parse_csv_file
     painter_object_array = []
     csv_data.each do |painter_row_arr|
         painter_row_arr.shift
+        byebug
         painter_object = {
             :name => painter_row_arr[0],
             :years => painter_row_arr[1],
@@ -32,11 +33,33 @@ def parse_csv_file
     painter_object_array.flatten
 end
 
-painters = parse_csv_file
+painters = parse_painter_csv_file
 # byebug
 painters.each do |painter_hash| 
     Painter.create!(painter_hash)
 end 
+
+# parsing paintings csv
+def parse_painting_csv_file
+    csv_data = CSV.read("db/csv_paintings.csv")
+    # iterate over each element and send back a hash 
+    # need to shift again at the beginning to get rid of id on the row
+    painter_object_array = []
+    csv_data.each do |painter_row_arr|
+        byebug
+        painter_row_arr.shift
+        painter_object = {
+            :name => painter_row_arr[0],
+            :years => painter_row_arr[1],
+            :genre => painter_row_arr[2],
+            :nationality => painter_row_arr[3],
+            :bio => painter_row_arr[4],
+            :painting_num => painter_row_arr[6]
+        }
+            painter_object_array.push(painter_object)  
+    end
+    painter_object_array.flatten
+end
 
 katherine = User.create(username: "Elronia")
 patrick = User.create(username: "pierre2")
