1085d11a6350a58d9998363c5c0b22a7730b41fa
pierrewebdev <patrick.pierre000@gmail.com>
2020-09-29 09:47:25 -0400

    fixed error in User controller so that JSON of users from our User table is properly sent out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     def handle_login
         user = User.find_user(params[:username])
+        render json: user
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
     has_many :paintings, through: :favorites
 
     def self.find_user(username)
-        find_user = User.find_or_create_by(username)
+        find_user = User.find_by(username:username)
+
+        if find_user
+            return find_user
+        else
+            User.create(username:username)
+        end
     end
 end
